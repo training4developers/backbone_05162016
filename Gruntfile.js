e308030855732f12c7b3ad4ac2aa564c387cf6ea
Eric W. Greene <eric@ericwgreene.com>
2016-05-13 18:12:31 -0400

    added initia files

---
+++ Gruntfile.js
@@ -0,0 +1,116 @@
+module.exports = function(grunt) {
+
+	const webpack = require('webpack');
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				options: {
+					sourcemap: 'none'
+				},
+				files: {
+					'./dist/www/css/site.css': './src/www/css/site.scss'
+				}
+			}
+		},
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true,
+						cwd: './src',
+						src: ['*.json'],
+						dest: './dist'
+					},
+					{
+						expand: true,
+						cwd: './src/www',
+						src: ['*.html'],
+						dest: './dist/www'
+					},
+					{
+						expand: true,
+						cwd: './src/www/tpls',
+						src: ['*.html'],
+						dest: './dist/www/tpls'
+					}
+				]
+			}
+		},
+		babel: {
+			options: {
+				presets: ['es2015']
+			},
+			js: {
+				files: [{
+					expand: true,
+					cwd: 'src',
+					src: ['**/*.js','!www/**'],
+					dest: 'dist',
+					ext: '.js'
+				}]
+			}
+		},
+		webpack: {
+			app: {
+				entry: './src/www/js/index.js',
+				output: {
+					path: './dist/www/js',
+					filename: 'index.js'
+				},
+				module: {
+					loaders: [{
+						test: /\.js$/,
+						exclude: /node_modules/,
+						loader: 'babel-loader',
+						query: {
+							passPerPreset: true,
+							presets: ['react', 'es2015']
+						}
+					}]
+				},
+				plugins: [
+					new webpack.ProvidePlugin({
+						'Promise': 'exports?global.Promise!es6-promise',
+						'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch',
+						'window.fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+					})
+				]
+			}
+		},
+		watch: {
+			sass: {
+				files: ['src/www/css/**/*.scss'],
+				tasks: ['sass']
+			},
+			copy: {
+				files: ['src/*.json','src/www/*.html','src/www/tpls/*.html'],
+				tasks: ['copy']
+			},
+			babel: {
+				files: ['src/**/*.js','!src/www/**'],
+				tasks: ['babel']
+			},
+			webpack: {
+				files: ['src/www/**/*.js'],
+				tasks: ['webpack']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-webpack');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('server', function() {
+		require('./dist/server').default(grunt.config());
+		this.async();
+	});
+
+	grunt.registerTask('default', [
+		'sass', 'copy', 'babel', 'webpack', 'watch'
+	]);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# backbone_05162016
+# Welcome to TechStars Node/Backbone!
+
+## Instructor
+
+Eric Greene - [http://t4d.io](http://t4d.io) - [LinkedIn](https://www.linkedin.com/in/ericwgreene)
+
+## Schedule
+
+Class:
+- Monday - Friday - 9:30am to 4:30pm
+
+Breaks:
+- Morning: 10:50am - 11:00am
+- Lunch: 12:15pm to 1pm
+- Afternoon #1: 2:05pm-2:15pm
+- Afternoon #2: 3:20pm-2:30pm
+
+## Course Outline
+
+- Day 1 - Backbone/Marionette
+- Day 2 - Backbone/Marionette
+- Day 3 - Node/Hapi/SocketIO
+- Day 4 - Node/Hapi/SocketIO
+
+## Links
+
+### Instructor's Resources
+
+- [Eric's Blog](http://t4d.io/)
+- [WintellectNOW](https://www.wintellectnow.com/Home/Instructor?instructorId=EricGreene) - Special Offer Code: GREENE-2016
+- [React Blog Posts](https://github.com/training4developers/react-flux-blog)
+- [TopTal Angular Directive](https://www.toptal.com/angular-js/angular-js-demystifying-directives)
+- [JavaScript Videos](https://www.toptal.com/videos)
+- [React SitePoint](http://www.sitepoint.com/author/ericgreene/)
+
+### Other Resources
+
+- [DevelopIntelligence](http://www.developintelligence.com/)
+- [You Don't Know JS](https://github.com/getify/You-Dont-Know-JS)
+- [JavaScript Air Podcast](http://javascriptair.podbean.com/)
+
+## Setup Instructions
+
+Prerequisites: [Node.js](https://nodejs.org/en/) (version 4 or later) and [Git](https://git-scm.com/) need to be installed, if they are not already
+
+Step 1: Using Git, clone the class repository: https://github.com/training4developers/backbone_05162016.git
+
+Step 2: From the terminal, change to the repository folder.
+
+Step 3: From the terminal, run the following commands:
+
+```bash
+$ npm i
+
+$ npm i -g grunt eslint eslint-plugin-react webpack babel-eslint
+
+$ grunt
+```
+If you prefer to use Gulp, then substitute 'gulp' for 'grunt' in the above command.
+
+Step 4: Open a new terminal window, change to the project folder.
+
+Step 5: From the terminal, run the following command:
+
+```bash
+$ grunt server
+```
+If you prefer to use Gulp, then substitute 'gulp' for 'grunt' in the above command.
+
+Step 6: Open a web browser, and navigate to http://localhost:3000
